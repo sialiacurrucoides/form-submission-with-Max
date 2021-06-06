758b3ad06681126f0d5003f967e7c810062ada7e
Simon Júlia <simon.julia@nexogen.hu>
2021-06-06 20:17:09 +0200

    improved modal with portals

--- src/components/UI/ErrorModal/ErrorModal.js
+++ src/components/UI/ErrorModal/ErrorModal.js
@@ -1,16 +1,15 @@
+import ReactDOM from 'react-dom';
+
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 import styles from './ErrorModal.module.scss';
 
-const ErrorModal = ({title, message, clearError}) => {
+const Backdrop = ({onClick}) => {
+    return <div className={styles.backdrop} onClick={onClick}></div>;
+};
 
-    const handleClick = () => {
-        clearError();
-    };
-
+const ModalOverlay = ({title, message, handleClick}) => {
     return (
-        <>
-        <div className={styles.backdrop} />
         <Card className={styles.modal}>
             <header className={styles.header}>
                 <h2>{title}</h2>
@@ -22,6 +21,24 @@ const ErrorModal = ({title, message, clearError}) => {
                 <Button onClick={handleClick}>Okay</Button>
             </footer>
         </Card>
+    );
+};
+
+const ErrorModal = ({title, message, clearError}) => {
+
+    const handleClick = () => {
+        clearError();
+    };
+
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop onClick={handleClick}/>, 
+            document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay title={title} 
+            message={message}
+            handleClick={handleClick}
+            />, 
+            document.getElementById('overlay-root'))}
         </>
     )
 };

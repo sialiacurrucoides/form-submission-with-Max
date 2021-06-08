50b17100a2ef900555a22b80f53054ebc71f46ed
Simon Júlia <simon.julia@nexogen.hu>
2021-06-08 10:14:51 +0200

    using Refs to handle user input

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 import styles from './AddUser.module.scss';
 import Card from '../UI/Card/Card';
@@ -7,41 +7,36 @@ import Button from '../UI/Button/Button';
 import ErrorModal from '../UI/ErrorModal/ErrorModal';
 
 const AddUser = ({addUser}) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0){
             setError({
                 title: "Invalid input",
                 message: "Please enter a valid name and age (non-empty value)."
             });
             return;
         }
-        if (Number(enteredAge) < 0){
+        if (Number(enteredUserAge) < 0){
             setError({
                 title: "Invalid age",
                 message: "The age should be a positive number."
             });
             return;
         }
-        setEnteredUsername('');
-        setEnteredAge('');
-        addUser(enteredUsername, enteredAge);
+        addUser(enteredName, enteredUserAge);
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
 
     const clearError = () => {
         setError();
-    };
-
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
-
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
     };
 
     return (
@@ -50,9 +45,17 @@ const AddUser = ({addUser}) => {
     <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" onChange={usernameChangeHandler} value={enteredUsername}/>
+            <input type="text" 
+            id="username" 
+            ref={nameInputRef}
+            />
             <label htmlFor="age">Age</label>
-            <input type="number" id="age" onChange={ageChangeHandler} value={enteredAge}/>
+            <input 
+            type="number" 
+            id="age" 
+            
+            ref={ageInputRef}
+            />
             <Button type="submit">Add user</Button>
         </form>
     </Card>
